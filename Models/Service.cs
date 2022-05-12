cd0db5277f0a183fd3d5b3bbddef133c945d5777
O-Okasha <deviancesplit2@gmail.com>
2022-05-12 22:58:51 +0200

    models bardo

--- Models/Service.cs
+++ Models/Service.cs
@@ -8,5 +8,27 @@ namespace Models
 {
     internal class Service
     {
+        public Service(int id, string name, string description, int price)
+        {
+            Id = id;
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Description = description ?? throw new ArgumentNullException(nameof(description));
+            this.price = price;
+        }
+        public Service(Service service)
+        {
+            if (service == null)
+            {
+                throw new ArgumentNullException(nameof(service));
+            }
+            Id = service.Id;
+            Name = service.Name;
+            Description = service.Description;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int price { get; set; }
     }
 }

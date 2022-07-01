9849c0a78f871ccaf2351949c6e8e9c40730bbd6
O-Okasha <deviancesplit2@gmail.com>
2022-07-01 20:35:02 +0200

    ss

--- Models/Medicine.cs
+++ Models/Medicine.cs
@@ -8,14 +8,15 @@ namespace Models
 {
     internal class Medicine
     {
-        public Medicine(string commercialName, string activeSubstance, float price, string id)
+        public Medicine(string commercialName, string activeSubstance, float price, string id, string _class)
         {
             CommercialName = commercialName ?? throw new ArgumentNullException(nameof(commercialName));
             ActiveSubstance = activeSubstance ?? throw new ArgumentNullException(nameof(activeSubstance));
             this.price = price;
             Id = id ?? throw new ArgumentNullException(nameof(id));
+            Class = _class;
         }
-
+        public string Class { get; set; }
         public string CommercialName { get; set; }
         public string ActiveSubstance { get; set; }
         public float price { get; set; }
